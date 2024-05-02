7171c525c6387fcbbfd080f9e7221e0deb1a454f
Vimal Paliwal (vim) <vimalpaliwal.me@gmail.com>
2024-05-02 11:23:03 +0100

    add more rules

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+locals {
+  rule_group_capacity = flatten(
+    [
+      lookup(var.block_sanctioned_countries, "enabled", false) ? [0] : [0]
+    ]
+  )
+}
+
 resource "aws_wafv2_rule_group" "example" {
   name        = var.rule_group_name
   description = var.rule_group_description
@@ -25,6 +33,248 @@ resource "aws_wafv2_rule_group" "example" {
         content {
           cloudwatch_metrics_enabled = true
           metric_name                = "${var.rule_group_name}-block-sanctioned-countries-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.block_cloudfront_default_domain, "enabled", false) ? [0] : []
+    content {
+      name     = "block-cloudfront-default-domain"
+      priority = var.block_cloudfront_default_domain["priority"]
+
+      action {
+        block {}
+      }
+
+      statement {
+        byte_match_statement {
+          field_to_match {
+            single_header {
+              name = "host"
+            }
+          }
+          positional_constraint = "ENDS_WITH"
+          search_string         = ".cloudfront.net"
+          text_transformation {
+            type     = "NONE"
+            priority = 0
+          }
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.block_cloudfront_default_domain["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-block-cloudfront-default-domain-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.block_load_balancer_default_domain, "enabled", false) ? [0] : []
+    content {
+      name     = "block-load-balancer-default-domain"
+      priority = var.block_load_balancer_default_domain["priority"]
+
+      action {
+        block {}
+      }
+
+      statement {
+        byte_match_statement {
+          field_to_match {
+            single_header {
+              name = "host"
+            }
+          }
+          positional_constraint = "ENDS_WITH"
+          search_string         = ".elb.amazonaws.com"
+          text_transformation {
+            type     = "NONE"
+            priority = 0
+          }
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.block_load_balancer_default_domain["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-block-load-balancer-default-domain-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.enable_aws_managed_common_rule_set, "enabled", false) ? [0] : []
+    content {
+      name     = "aws-managed-common-rule-set"
+      priority = var.enable_aws_managed_common_rule_set["priority"]
+
+      override_action {
+        none {}
+      }
+
+      statement {
+        managed_rule_group_statement {
+          name        = "AWSManagedRulesCommonRuleSet"
+          vendor_name = "AWS"
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.enable_aws_managed_common_rule_set["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-aws-managed-common-rule-set-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.enable_aws_known_bad_input_rule_set, "enabled", false) ? [0] : []
+    content {
+      name     = "aws-known-bad-input-rule-set"
+      priority = var.enable_aws_known_bad_input_rule_set["priority"]
+
+      override_action {
+        none {}
+      }
+
+      statement {
+        managed_rule_group_statement {
+          name        = "AWSManagedRulesKnownBadInputsRuleSet"
+          vendor_name = "AWS"
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.enable_aws_known_bad_input_rule_set["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-aws-known-bad-input-rule-set-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.enable_aws_ip_reputation_rule_set, "enabled", false) ? [0] : []
+    content {
+      name     = "aws-ip-reputation-rule-set"
+      priority = var.enable_aws_ip_reputation_rule_set["priority"]
+
+      override_action {
+        none {}
+      }
+
+      statement {
+        managed_rule_group_statement {
+          name        = "AWSManagedRulesAmazonIpReputationList"
+          vendor_name = "AWS"
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.enable_aws_ip_reputation_rule_set["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-aws-ip-reputation-rule-set-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.enable_aws_sql_injection_rule_set, "enabled", false) ? [0] : []
+    content {
+      name     = "aws-sql-injection-rule-set"
+      priority = var.enable_aws_sql_injection_rule_set["priority"]
+
+      override_action {
+        none {}
+      }
+
+      statement {
+        managed_rule_group_statement {
+          name        = "AWSManagedRulesSQLiRuleSet"
+          vendor_name = "AWS"
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.enable_aws_sql_injection_rule_set["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-aws-sql-injection-rule-set-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.enable_aws_linux_rule_set, "enabled", false) ? [0] : []
+    content {
+      name     = "aws-linux-rule-set"
+      priority = var.enable_aws_linux_rule_set["priority"]
+
+      override_action {
+        none {}
+      }
+
+      statement {
+        managed_rule_group_statement {
+          name        = "AWSManagedRulesLinuxRuleSet"
+          vendor_name = "AWS"
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.enable_aws_linux_rule_set["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-aws-linux-rule-set-rule-waf"
+          sampled_requests_enabled   = true
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = lookup(var.enable_aws_windows_rule_set, "enabled", false) ? [0] : []
+    content {
+      name     = "aws-windows-rule-set"
+      priority = var.enable_aws_windows_rule_set["priority"]
+
+      override_action {
+        none {}
+      }
+
+      statement {
+        managed_rule_group_statement {
+          name        = "AWSManagedRulesWindowsRuleSet"
+          vendor_name = "AWS"
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = var.enable_aws_windows_rule_set["enable_cw_metrics"] ? [0] : []
+        content {
+          cloudwatch_metrics_enabled = true
+          metric_name                = "${var.rule_group_name}-aws-windows-rule-set-rule-waf"
           sampled_requests_enabled   = true
         }
       }
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "rule_group_name" {
 
 variable "rule_group_description" {
   type        = string
-  default     = "Essential security rule group"
+  default     = "Baseline security WAF rule group"
   description = "Description for the rule group"
 }
 
@@ -48,22 +48,125 @@ variable "block_sanctioned_countries" {
   description = "Blacklist all incoming traffic from the countries sanctioned by the US"
 }
 
-# variable "block_aws_default_domain" {
-#   type = object({
-#     enabled          = bool
-#     cloudfront       = bool
-#     load_balancer    = bool
-#     http_api_gateway = bool
-#   })
+variable "block_cloudfront_default_domain" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
 
-#   default = {
-#     enabled          = true
-#     cloudfront       = true
-#     load_balancer    = true
-#     http_api_gateway = true
-#   }
-#   description = "Block all incoming traffic if the request host header contains aws service default domain. This rule prevents bad actors from bypassing the custom domain to which you have mapped the AWS service"
-# }
+  default = {
+    enabled           = true
+    priority          = 1
+    enable_cw_metrics = true
+  }
+  description = "Block all incoming traffic if the request host header contains cloudfront domain. This rule prevents bad actors from bypassing the custom domain to which you have mapped cloudfront domain"
+}
+
+variable "block_load_balancer_default_domain" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
+
+  default = {
+    enabled           = true
+    priority          = 2
+    enable_cw_metrics = true
+  }
+  description = "Block all incoming traffic if the request host header contains load balancer domain. This rule prevents bad actors from bypassing the custom domain to which you have mapped load balancer domain"
+}
+
+variable "enable_aws_managed_common_rule_set" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
+
+  default = {
+    enabled           = true
+    priority          = 3
+    enable_cw_metrics = true
+  }
+  description = "Add [AWSManagedRulesCommonRuleSet](https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-baseline.html#aws-managed-rule-groups-baseline-crs) to the rule group. Protects against wide range of vulnerabilities described in OWASP Top 10"
+}
+
+variable "enable_aws_known_bad_input_rule_set" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
+
+  default = {
+    enabled           = true
+    priority          = 4
+    enable_cw_metrics = true
+  }
+  description = "Add [AWSManagedRulesKnownBadInputsRuleSet](https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-baseline.html#aws-managed-rule-groups-baseline-known-bad-inputs) to the rule group. Protects against malicious request patterns to prevent discovery of vulnerabilities"
+}
+
+variable "enable_aws_ip_reputation_rule_set" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
+
+  default = {
+    enabled           = true
+    priority          = 5
+    enable_cw_metrics = true
+  }
+  description = "Add [AWSManagedRulesAmazonIpReputationList](https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-ip-rep.html#aws-managed-rule-groups-ip-rep-amazon) to the rule group. Protects against requests origination from know bot IPs"
+}
+
+variable "enable_aws_sql_injection_rule_set" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
+
+  default = {
+    enabled           = true
+    priority          = 6
+    enable_cw_metrics = true
+  }
+  description = "Add [AWSManagedRulesSQLiRuleSet](https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-use-case.html#aws-managed-rule-groups-use-case-sql-db) to the rule group. Protects against malicious requests trying to exploit SQL vulnerabilities"
+}
+
+variable "enable_aws_linux_rule_set" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
+
+  default = {
+    enabled           = true
+    priority          = 7
+    enable_cw_metrics = true
+  }
+  description = "Add [AWSManagedRulesLinuxRuleSet](https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-use-case.html#aws-managed-rule-groups-use-case-linux-os) to the rule group. Protects against requests trying to exploit Linux specific vulnerabilities"
+}
+
+variable "enable_aws_windows_rule_set" {
+  type = object({
+    enabled           = bool
+    priority          = number
+    enable_cw_metrics = bool
+  })
+
+  default = {
+    enabled           = true
+    priority          = 8
+    enable_cw_metrics = true
+  }
+  description = "Add [AWSManagedRulesWindowsRuleSet](https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-use-case.html#aws-managed-rule-groups-use-case-windows-os) to the rule group. Protects against requests trying to exploit Windows specific vulnerabilities"
+}
 
 variable "tags" {
   type        = map(string)
